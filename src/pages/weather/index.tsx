d3ffbaf2a269bc57c962846a686e8043829a5927
Croked1991 <atik.egor@gmail.com>
2022-11-10 17:42:48 +0300

    apply changes on heroku test

--- src/pages/weather/index.tsx
+++ src/pages/weather/index.tsx
@@ -54,7 +54,7 @@ export const Weather = () => {
                         ?
                         mapWeather
                         :
-                        <TextCard className='textCard-text'>CI/CD works</TextCard>
+                        <TextCard className='textCard-text'>CI/CD works sds</TextCard>
                     }
                 </Flex>
             </Flex>
